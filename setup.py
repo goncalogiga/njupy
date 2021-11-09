ea37f6d1f5f2eda989810905730f3cc54c71e0bc
CASIMIRO-MATIAS-MAIA-GIGA GONCALO <goncalo.casimiro-matias-maia-giga@etu.unistra.fr>
2021-11-09 23:32:42 +0100

    cleaning checkpoints

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
     install_requires=['jupyter_ascending',
                       'jupytext',
                       'click',
+                      'jsonrpcclient < 4.0.0',
                       ],
     scripts=['bin/njupy']
 )
